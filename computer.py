502e41102a7afea928a18a7032c6143de54201c7
Ellory <edoyle@smith.edu>
2023-02-08 22:14:27 -0500

    Here are my hw changes

--- computer.py
+++ computer.py
@@ -2,13 +2,14 @@
     Filename: computer.py
     Description: the class definitions for Computer
     Author: Ellory Doyle (@ellorydoyle)
-    Date: 6 February 2023
+    Date: 8 February 2023
 """
 # Import a few useful containers from the typing module
 from typing import Dict, Union, Optional
+#This class represents my computers
 class Computer:
 
-    # What attributes will it need?
+    # Here are my attributes
     description: str
     processor_type: str
     hard_drive_capacity: int
@@ -17,8 +18,8 @@ class Computer:
     year_made: int
     price: int
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
+    #Here are my methods
+    #This is my constructor
     def __init__(self, description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price) -> None:
         self.description = description
         self.processor_type = processor_type
@@ -28,26 +29,14 @@ class Computer:
         self.year_made = year_made
         self.price = price
     
-    def print_details(self):
-        print(self.description)
-        print(self.processor_type)
-        print(self.hard_drive_capacity)
-        print(self.memory)
-        print(self.operating_system)
-        print(self.year_made)
-        print(self.price)
-
-    # What methods will you need?
-    
-def main():
-    description = input("What is the description of the computer?  > ")
-    processor_type = input("What is the processor type of the computer?  > ")
-    hard_drive_capacity = input("What is the hard drive capacity of the computer?  > ")
-    memory = input("How big is the computer's memory?  > ")
-    operating_system = input("What is the operating system of the computer?  > ")
-    year_made = input("What is the year that the computer was made?  > ")
-    price = input("What is the price of the computer?  > ")
-    computer = Computer(description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price)
-    computer.print_details()
-
-main()
+    #This can be called on to print or return the computer details
+    def __repr__(self) -> str:
+        return (
+        f"Description: {self.description}\n"
+        f"Processor Type: {self.processor_type}\n"
+        f"Hard Drive Capacity: {self.hard_drive_capacity}\n"
+        f"Memory: {self.memory}\n"
+        f"Operating System: {self.operating_system}\n"
+        f"Year Made: {self.year_made}\n"
+        f"Price: {self.price}"
+        )
