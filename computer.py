fed3e624b2da52da6af9cd02e498f3e85fcd9c84
Ellory <edoyle@smith.edu>
2023-02-08 22:03:13 -0500

    This is my final commit, hopefully! Edited computer.py and oo_resale_shop.py

--- computer.py
+++ computer.py
@@ -1,10 +1,53 @@
+"""
+    Filename: computer.py
+    Description: the class definitions for Computer
+    Author: Ellory Doyle (@ellorydoyle)
+    Date: 6 February 2023
+"""
+# Import a few useful containers from the typing module
+from typing import Dict, Union, Optional
 class Computer:
 
     # What attributes will it need?
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price) -> None:
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made = year_made
+        self.price = price
+    
+    def print_details(self):
+        print(self.description)
+        print(self.processor_type)
+        print(self.hard_drive_capacity)
+        print(self.memory)
+        print(self.operating_system)
+        print(self.year_made)
+        print(self.price)
 
     # What methods will you need?
+    
+def main():
+    description = input("What is the description of the computer?  > ")
+    processor_type = input("What is the processor type of the computer?  > ")
+    hard_drive_capacity = input("What is the hard drive capacity of the computer?  > ")
+    memory = input("How big is the computer's memory?  > ")
+    operating_system = input("What is the operating system of the computer?  > ")
+    year_made = input("What is the year that the computer was made?  > ")
+    price = input("What is the price of the computer?  > ")
+    computer = Computer(description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price)
+    computer.print_details()
+
+main()
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,44 @@
+"""
+    Filename: oo_resale_shop.py
+    Description: the class definitions for Resale Shop
+    Author: Ellory Doyle (@ellorydoyle)
+    Date: 6 February 2023
+"""
+from computer import *
+from typing import Dict, Union, Optional
 class ResaleShop:
 
     # What attributes will it need?
-
+    name = "Ellory's Store"
+    inventory = list
+    item_id = 0 # We'll increment this every time we add a new item 
+                # so that we always have a new value for the itemID
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, inventory) -> None:
+        self.inventory = []
 
     # What methods will you need?
+    def buy(self, inventory, item_id):
+        item_id += 1
+        item_id = Computer()
+        inventory.append(item_id.computer)
+        print(self.inventory)
+        #1. call Computer(...)constructor
+        #   to create a new Computer instance
+
+        #2. call inventory.append(...) to add the
+        #   new Computer instance to the inventory
+
+    def sell(self, inventory, item_id):
+        pass
+
+    def update_price(self, inventory, price):
+        pass
+
+    def update_os(self, inventory):
+        pass
+
+    def print_inventory(self):
+        for c in self.inventory:
+            c.print_details()
